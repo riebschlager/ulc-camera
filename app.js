74423dd4bfdb21354ea0d9e5fab4f7e8aa8ddd79
Chris Riebschlager <criebschlager@dimin.com>
2018-06-05 20:20:56 -0500

    wip checkpoint

--- app.js
+++ app.js
@@ -1,56 +1,71 @@
 'use strict';
 
 const dir = '/home/pi/ulc-camera/';
+const config = require('config');
+const aws = require('aws-sdk');
+const cp = require('child_process');
+const fs = require('fs');
+const io = require('socket.io-client');
+const cameraId = config.get('cameraId');
 
-const AWS = require('aws-sdk');
-const CP = require('child_process');
-const FS = require('fs');
-const IO = require('socket.io-client');
-AWS.config.region = 'us-west-2';
+aws.config.region = 'us-west-2';
 
-let socket = IO.connect('ulc-relay.herokuapp.com', { port: 80 });
+const socket = io.connect(
+  'ulc-relay.herokuapp.com',
+  { port: 80 }
+);
 
 function takePhoto(boardId) {
-    let cmd = CP.exec('/usr/bin/fswebcam --no-banner -r 1920x1080 --jpeg 90 /home/pi/ulc-camera/board-image.jpg -S 30');
-    cmd.on('exit', () => {
-        let body = FS.createReadStream(dir + 'board-image.jpg');
-        let s3 = new AWS.S3({
-            params: {
-                Bucket: 'ulc.the816.co',
-                Key: boardId + '-' + Date.now() + '.jpg',
-                ContentType: 'image/jpeg',
-                Body: body
-            }
-        });
-        s3.upload().send(function(err, data) {
-            socket.emit('take-photo-complete', {
-                url: data.Location,
-                boardId: boardId
-            });
-        });
+  let cmd = cp.exec(
+    '/usr/bin/fswebcam --no-banner -r 1920x1080 --jpeg 90 /home/pi/ulc-camera/board-image.jpg -S 30'
+  );
+  cmd.on('exit', () => {
+    let body = fs.createReadStream(dir + 'board-image.jpg');
+    let s3 = new aws.S3({
+      params: {
+        Bucket: 'ulc.the816.co',
+        Key: `${boardId}-${cameraId}-${Date.now()}.jpg`,
+        ContentType: 'image/jpeg',
+        Body: body
+      }
     });
+    s3.upload().send((err, data) => {
+      socket.emit('take-photo-complete', {
+        url: data.Location,
+        boardId: boardId,
+        cameraId: cameraId
+      });
+    });
+  });
 }
 
-let heartbeat = setInterval(function() {
-    socket.emit('heartbeat', { time: Date.now() });
+let heartbeat = setInterval(() => {
+  socket.emit('heartbeat', {
+    time: Date.now(),
+    cameraId: cameraId
+  });
 }, 1000);
 
-socket.on('deploy', function() {
-    clearInterval(heartbeat);
-    CP.execSync('cd ' + dir + ' && git pull');
-    CP.exec('sudo reboot');
+socket.on('deploy', msg => {
+  if (parseInt(msg.cameraId) !== cameraId) return;
+  clearInterval(heartbeat);
+  cp.execSync('cd ' + dir + ' && git pull');
+  cp.exec('sudo reboot');
 });
 
-socket.on('reboot', function() {
-    clearInterval(heartbeat);
-    CP.exec('sudo reboot');
+socket.on('reboot', msg => {
+  if (parseInt(msg.cameraId) !== cameraId) return;
+  clearInterval(heartbeat);
+  cp.exec('sudo reboot');
 });
 
-socket.on('shutdown', function() {
-    clearInterval(heartbeat);
-    CP.exec('sudo shutdown -h now');
+socket.on('shutdown', msg => {
+  if (parseInt(msg.cameraId) !== cameraId) return;
+  clearInterval(heartbeat);
+  cp.exec('sudo shutdown -h now');
 });
 
-socket.on('take-photo', function(msg) {
-    takePhoto(msg.boardId);
+socket.on('take-photo', msg => {
+  if (parseInt(msg.cameraId) !== cameraId) return;
+  takePhoto(msg.boardId);
 });
